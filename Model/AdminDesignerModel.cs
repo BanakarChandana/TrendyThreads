fad6dd2fb552e125bdfc0ab91279ec7b81e06deb
BanakarChandana <chandanabanakar2@outlook.com>
2026-03-13 20:00:11 +0530

    added product cards in dashboard

--- Model/AdminDesignerModel.cs
+++ Model/AdminDesignerModel.cs
@@ -1,16 +1,21 @@
-﻿namespace ThrendyThreads.Model
+﻿using System;
+
+namespace ThrendyThreads.Model
 {
     public class AdminDesignerModel
     {
-        // Registration
+        // --------------------------------
+        // Registration Details
+        // --------------------------------
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         public byte[]? Image { get; set; }
 
-        // Designer
+        // --------------------------------
+        // Designer Details
+        // --------------------------------
         public string DesignerName { get; set; }
-        public byte[]? DesignerImage { get; set; }
         public string AboutDesigner { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
